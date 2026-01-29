c466c37b8f9b014e6233514de14bce1ef25d489f
AdminManuelSolis <abogadosmanuelsolis@gmail.com>
2026-01-29 09:24:52 -0600

    update

--- Carrito_De_Compras/src/App.jsx
+++ Carrito_De_Compras/src/App.jsx
@@ -1,272 +1,130 @@
 
 import Header from './components/Header.jsx'
+import Guitar from './components/Guitar.jsx'
+import {db} from './data/db.js'
+import {useEffect, useState} from 'react'
 
 function App() { //Los componentes son funciones y estos siempre deben de comenzar por mayuscula
+    //USO DE useState
+    //const [autenticador, setAutenticador] = useState([])
 
-  return (
-    <>
-    <Header />
-    <header class="py-5 header">
-        <div class="container-xl">
-            <div class="row justify-content-center justify-content-md-between">
-                <div class="col-8 col-md-3">
-                    <a href="index.html">
-                        <img class="img-fluid" src="./public/img/logo.svg" alt="imagen logo" />
-                    </a>
-                </div>
-                <nav class="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                    <div 
-                        class="carrito"
-                    >
-                        <img class="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+    //USO DE useEffect
+    //useEffect (() => {
+      //  console.log('Componente listo')
+    //},[autenticador])
 
-                        <div id="carrito" class="bg-white p-3">
-                            <p class="text-center">El carrito esta vacio</p>
-                            <table class="w-100 table">
-                                <thead>
-                                    <tr>
-                                        <th>Imagen</th>
-                                        <th>Nombre</th>
-                                        <th>Precio</th>
-                                        <th>Cantidad</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <img class="img-fluid" src="./public/img/guitarra_02.jpg" alt="imagen guitarra" />
-                                        </td>
-                                        <td>SRV</td>
-                                        <td class="fw-bold">
-                                                $299
-                                        </td>
-                                        <td class="flex align-items-start gap-4">
-                                            <button
-                                                type="button"
-                                                class="btn btn-dark"
-                                            >
-                                                -
-                                            </button>
-                                                1
-                                            <button
-                                                type="button"
-                                                class="btn btn-dark"
-                                            >
-                                                +
-                                            </button>
-                                        </td>
-                                        <td>
-                                            <button
-                                                class="btn btn-danger"
-                                                type="button"
-                                            >
-                                                X
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+    //Inicializar la database
+    const initialCart = () => {
+        const localStorageCart = localStorage.getItem('cart')
+        return localStorageCart ? JSON.parse(localStorageCart) : []
+    }
 
-                            <p class="text-end">Total pagar: <span class="fw-bold">$899</span></p>
-                            <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
-                        </div>
-                    </div>
-                </nav>
+    const [data] = useState(db) //Su valor inicial es el arreglo de informacion con el que contamos 
+    const [cart, setCart] = useState([initialCart]) //Creamos un prop para poner tomar los datos y mandalros al carrito
+
+     //Funcion para tener un guardado local
+    useEffect(() =>{
+        //Solo guarda strings
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart]) // De esta manera le decimos que solo haga esta accion cuando el evento ya haya finalizado
+
+
+    function addToCart(item){
+        //findIndex identifica si el iteam ya existe de no ser asi, manda un -1, si eciste manda la posicion de dicho item
+        const itemExist = cart.findIndex((guitar) => guitar.id === item.id)
+
+        if(itemExist >= 0 ){ //Verifica si el iteam ya existe
+            if(cart[itemExist].quantity >= 5) return
+            const updateCart = [...cart] //Esta es una copia del carrito
+            updateCart[itemExist].quantity++
+            setCart(updateCart) //Le enviamos todo el carrito nuevamente
+        }else{
+            item.quantity = 1 //Agregamos una propiedad que nos indica la cantodad de ese mismo item
+            setCart([...cart, item])
+        }
+
+    }
+
+    //Funcion para eliminar un item del carrito
+    function removeFromCart(id){
+        //Buscamos los elementos diferentes, debido a que solo queremos eliminar 
+        //el que se esta presionando
+        setCart(prevCart => prevCart.filter(guitar => guitar.id != id))
+    }
+
+    //Uso de boton +
+    function increaseQuantity(id){
+        const updateCart = cart.map(item => {
+            if(item.id === id && item.quantity < 5){
+                return{
+                    ...item, //Copia todo nuestro item y solo modifica
+                    quantity: item.quantity + 1
+                }
+            }
+            return item
+        })
+        setCart(updateCart)
+    }
+
+    //Reto 1 decremento de cantidades 
+    function decrementQuantity(id){
+        const updateCart = cart.map(item => {
+            if(item.id === id && item.quantity > 1){
+                return{
+                    ...item,
+                    quantity: item.quantity -1
+                }
+            }
+            return item
+        })
+        setCart(updateCart)
+    }
+
+    //Funcion que nos ayuda a limpiar el carrito
+    function clearCart (){
+        setCart ([])
+    }
+
+   
+
+    return (
+        <>
+        <Header 
+            cart = {cart}
+            removeFromCart = {removeFromCart}
+            increaseQuantity = {increaseQuantity}
+            decrementQuantity = {decrementQuantity}
+            clearCart = {clearCart}
+        />
+
+        <main className= "container-xl mt-5">
+            <h2 className= "text-center">Nuestra Colección</h2>
+
+            {/* Row funciona de manera que alinea los elementos en filas */}
+            <div className= "row mt-5">
+                {/*Lo que haremos es com .map crear un arreglo por cada objeto, creando un componente*/}
+                {/*Cada que iteramos usando .map,*/}
+                {data.map((guitar) => ( //Podemos tomar diferentes objetos
+                    <Guitar
+                        /*Cada que iteramos usando .map, necesitamos un id unico llamdo key*/
+                        key={guitar.id}
+                        guitar={guitar} //Asignamos el objeto a un promp
+                        setCart={setCart}
+                        addToCart={addToCart}
+                    /> 
+                ))}
             </div>
-        </div>
-    </header>
-
-    <main class="container-xl mt-5">
-        <h2 class="text-center">Nuestra Colección</h2>
-
-        <div class="row mt-5">
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_01.jpg" alt="imagen guitarra" />
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100"
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_02.jpg" alt="imagen guitarra"/>
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">SRV</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_03.jpg" alt="imagen guitarra"/>
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Borland</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_04.jpg" alt="imagen guitarra"/>
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Vai</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_05.jpg" alt="imagen guitarra"/>
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Thompson</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_06.jpg" alt="imagen guitarra"/>
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">White</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_07.jpg" alt="imagen guitarra"/>
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Cobain</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_08.jpg" alt="imagen guitarra"/>
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Dale</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_09.jpg" alt="imagen guitarra"/>
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Krieger</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_10.jpg" alt="imagen guitarra"/>
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Campbell</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_11.jpg" alt="imagen guitarra"/>
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Reed</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_12.jpg" alt="imagen guitarra"/>
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Hazel</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-        </div>
-    </main>
+        </main>
 
 
-    <footer class="bg-dark mt-5 py-5">
-        <div class="container-xl">
-            <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-        </div>
-    </footer>
+        <footer className= "bg-dark mt-5 py-5">
+            <div className= "container-xl">
+                <p className= "text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+            </div>
+        </footer>
 
-    </>
-  )
+        </>
+    )
 }
 
 export default App
--- Carrito_De_Compras/src/components/Header.jsx
+++ Carrito_De_Compras/src/components/Header.jsx
@@ -1,13 +1,111 @@
 //Aqui podemos importar librerias o componentes
+import { useMemo } from "react";
 
-function Header () {
+function Header ({cart, removeFromCart, increaseQuantity, decrementQuantity, clearCart}) {
     //Aqui van a ir nuestras states o funciones
-
-    const name = 'Monica'
+    //const name = 'Monica'
     //Todo lo que este dentro del return se muestra
+
+    //State Derivado
+    /*useMemo, nos permite que cuando se precione el boton no se haga un render de la aplicacion
+    completa, si no que solo de este state*/
+    const isEmpty = useMemo(() => cart.length === 0, [cart]) //Cuando el carrito cambie
+
+    /*Funcion para obtener el total del carrito y mandarlo a mostrar
+        total funciona como variable al igual que item, de manera que sumamos lo que ya tenemos*/
+    const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price), 0))
     return (
         //Para poder compilar codigo JS se necesita estar entre {}
-        <p>Nombre: {name}</p>
+        //<p>Nombre: {name}</p>
+        <header className= "py-5 header">
+        <div className= "container-xl">
+            <div className= "row justify-content-center justify-content-md-between">
+                <div className= "col-8 col-md-3">
+                    <a href="index.html">
+                        <img className= "img-fluid" src="./public/img/logo.svg" alt="imagen logo" />
+                    </a>
+                </div>
+                <nav className= "col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+                    <div 
+                        className= "carrito"
+                    >
+                        <img className= "img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+
+                        <div id="carrito" className= "bg-white p-3">
+                            
+                            {isEmpty ? (
+                                <p className= "text-center">El carrito esta vacio</p>
+                            ):(
+                            <>
+                                <table className= "w-100 table">
+                                    <thead>
+                                        <tr>
+                                            <th>Imagen</th>
+                                            <th>Nombre</th>
+                                            <th>Precio</th>
+                                            <th>Cantidad</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    {/*recordemos que de esta manera va a repetir el codigo
+                                        por cada elemento que tengamos en el arreglo*/}
+                                        {cart.map(guitar => (
+                                            <tr key={guitar.id}>
+                                                <td>
+                                                    <img className= "img-fluid" 
+                                                    src={`/img/${guitar.image}.jpg`}
+                                                    alt="imagen guitarra" />
+                                                </td>
+                                                <td>{guitar.name}</td>
+                                                <td className= "fw-bold">
+                                                        ${guitar.price}
+                                                </td>
+                                                <td className= "flex align-items-start gap-4">
+                                                    <button
+                                                        type="button"
+                                                        className= "btn btn-dark"
+                                                        onClick={() => decrementQuantity(guitar.id)}
+                                                    >
+                                                        -
+                                                    </button>
+                                                        {guitar.quantity}
+                                                    <button
+                                                        type="button"
+                                                        className= "btn btn-dark"
+                                                        onClick={() => increaseQuantity(guitar.id)}
+                                                    >
+                                                        +
+                                                    </button>
+                                                </td>
+                                                <td>
+                                                    <button
+                                                        className= "btn btn-danger"
+                                                        type="button"
+                                                        onClick={() => removeFromCart(guitar.id)}
+                                                    >
+                                                        X
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                                
+                                <p className= "text-end">Total pagar: <span className= "fw-bold">${cartTotal}</span></p>
+                            </>
+                        )}  
+                        <button 
+                        className= "btn btn-dark w-100 mt-3 p-2"
+                        onClick={clearCart}
+                        >
+                        Vaciar Carrito</button>                    
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
     );
 }
 
